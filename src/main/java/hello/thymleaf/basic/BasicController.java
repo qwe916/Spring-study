4f1763d1a5e487c07d82ea959ee25444436dde27
Hongyoung Jun <qwe916@naver.com>
2022-08-15 22:25:12 +0900

    thymeleaf

--- src/main/java/hello/thymleaf/basic/BasicController.java
+++ src/main/java/hello/thymleaf/basic/BasicController.java
@@ -120,6 +120,14 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/operation";
     }
+
+    /**
+     *
+     */
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
