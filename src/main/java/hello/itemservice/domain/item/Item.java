80c44f444207a1529515f25b0d30fef6611f1e0a
Hongyoung Jun <qwe916@naver.com>
2022-08-19 23:50:09 +0900

    enum, class의 체크 박스 활용

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,8 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
 
@@ -10,6 +12,10 @@ public class Item {
     private Integer price;
     private Integer quantity;
 
+    private Boolean open;//판매 여부
+    private List<String> regions;//등록 지역
+    private ItemType itemType;//상품 타입
+    private String deliverCode;//배송 코드
     public Item() {
     }
 
