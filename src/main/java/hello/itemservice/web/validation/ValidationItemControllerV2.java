ffbb239021b8533f31edaf6287c2f4fe6a1a760c
Hongyoung Jun <qwe916@naver.com>
2022-08-27 22:30:42 +0900

    오류코드 , errors.properties

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -120,7 +120,7 @@ public class ValidationItemControllerV2 {
 
         // BindingResult로 error 전달
         if (!StringUtils.hasText(item.getItemName())) {//글자가 없으면
-            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, "상품이름은 필수입니다."));
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.addError(new FieldError("item", "price",item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000,10000000}, "가격은 1,000~1,000,0000 까지 허용합니다."));
@@ -133,7 +133,7 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 //객체 필드가 아닌 경우 new ObjectError 생성, Object 에러는 넘어오는 값이 없기 때문에 입력된 값을 유지할 것이 없다.
-                bindingResult.addError(new ObjectError("item", null, null, "가격 x 수량의 합은 10,000원 이상이여햐 합니다. 현재 값 " + resultPrice));
+                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000,resultPrice}, "가격 x 수량의 합은 10,000원 이상이여햐 합니다. 현재 값 " + resultPrice));
             }
         }
 
