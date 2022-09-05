1b5f971cdf07f6aec32c8d2d5c89a4ceda132488
Hongyoung Jun <qwe916@naver.com>
2022-09-05 23:56:22 +0900

    Form 전송객체 분리

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -16,18 +16,18 @@ import javax.validation.constraints.NotNull;
  */
 @Data
 public class Item {
-    @NotNull(groups = UpdateCheck.class)//수정 요구 사항 추가 및 groups 추가
+    //@NotNull(groups = UpdateCheck.class)//수정 요구 사항 추가 및 groups 추가
     private Long id;
     //오류 메세지는 애노테이션 파라미터 message를 정의해준다.
-    @NotBlank(message = "공백 x", groups = {SaveCheck.class,UpdateCheck.class})//빈값 + 공백만 있는 경우를 허용하지 않는다.
+    //@NotBlank(message = "공백 x", groups = {SaveCheck.class,UpdateCheck.class})//빈값 + 공백만 있는 경우를 허용하지 않는다.
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})//null을 허용하지 않는다.
-    @Range(min = 1000, max=1000000,groups = {SaveCheck.class,UpdateCheck.class})//범위 설정
+    //@NotNull(groups = {SaveCheck.class,UpdateCheck.class})//null을 허용하지 않는다.
+    //@Range(min = 1000, max=1000000,groups = {SaveCheck.class,UpdateCheck.class})//범위 설정
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
-    @Max(value = 9999,groups = {SaveCheck.class}) //추가 요구 사항 추가
+    //@NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+    //@Max(value = 9999,groups = {SaveCheck.class}) //추가 요구 사항 추가
     // 최대값 설정
     private Integer quantity;
 
