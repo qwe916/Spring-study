7691b2faf841d5995bdd06c6beab3736c5c0c559
Hongyoung Jun <qwe916@naver.com>
2022-09-14 23:58:08 +0900

    Spring Interceptor(인증 체크)

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +20,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")//모든 경로에 인터셉터
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
-
+        /**
+         * 필터와 달리 따로 배열을 만들어서 제외할 URL을 지정할 필요 없고
+         * excludePathPatterns 메소드를 통해 제외 시킬 수 있다.
+         */
+        registry.addInterceptor(new LoginInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "*.ico");
     }
 
     //Filter Bean 등록
