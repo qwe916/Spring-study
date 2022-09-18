0955a088e4fb85a9dc081bbd879b69602fa34d4d
Hongyoung Jun <qwe916@naver.com>
2022-09-18 22:42:21 +0900

    서블릿 예외 처리(오류 화면 제공)

---
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.awt.image.renderable.ContextualRenderedImageFactory;
+//웹서버를 커스텀하는 인터페이스
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        // 에러 페이지 추가
+        //ErrorPage(에러, 페이지)
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
