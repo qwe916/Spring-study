54d341cc7af9832578324715ba881214f33a0f43
Hongyoung Jun <qwe916@naver.com>
2022-09-18 23:14:00 +0900

    Filter 예외처리

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -14,31 +14,15 @@ import java.awt.image.renderable.ContextualRenderedImageFactory;
 @Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
-    //RequestDispatcher 상수로 정의되어 있음
-    public static final String ERROR_EXCEPTION ="javax.servlet.error.exception";
-    public static final String ERROR_EXCEPTION_TYPE = "javax.servlet.error.exception_type";
-    public static final String ERROR_MESSAGE = "javax.servlet.error.message";
-    public static final String ERROR_REQUEST_URI = "javax.servlet.error.request_uri";
-    public static final String ERROR_STATUS_CODE = "javax.servlet.error.status_code";
-    public static final String ERROR_SERVLET_NAME = "javax.servlet.error.servlet_name";
-    @Override
+     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         // 에러 페이지 추가
         //ErrorPage(에러, 페이지)
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
         ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
-
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
     }
 
-    private void printErrorInfo(HttpServletRequest request) {
-        log.info("ERROR_EXCEPTION: ex=", request.getAttribute(ERROR_EXCEPTION));
-        log.info("ERROR_EXCEPTION_TYPE: {}",request.getAttribute(ERROR_EXCEPTION_TYPE));
-        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE)); //ex의 경우 NestedServletException 스프링이 한번 감싸서 반환
-        log.info("ERROR_REQUEST_URI: {}",request.getAttribute(ERROR_REQUEST_URI));
-        log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
-        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
-        log.info("dispatchType={}", request.getDispatcherType());
-    }
+
 }
