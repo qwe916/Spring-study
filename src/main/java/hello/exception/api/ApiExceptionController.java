61c3242b75e3586cafcb4a21f0e9eb3e4369b7d2
Hongyoung Jun <qwe916@naver.com>
2022-09-23 00:08:06 +0900

    ResponseStatusException 예외 처리

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,12 +1,15 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -27,9 +30,19 @@ public class ApiExceptionController {
         return new MemberDto(id, "hello" + id);
     }
 
+    @GetMapping("/api/reponse-status-ex1")
+    public String reponseStautsEx1() {
+        throw new BadRequestException();//400오류 발생
+    }
+
+    @GetMapping("/api/reponse-stauts-ex2")
+    public String reponseStatusEx2(){
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
     @Data
     @AllArgsConstructor
-    static class MemberDto{
+    static class MemberDto {
         private String name;
         private String memberId;
     }
