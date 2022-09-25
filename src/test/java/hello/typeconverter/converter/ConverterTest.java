dda2d7323055c0c3f52fbf899b54fb249ba9fb2e
Hongyoung Jun <qwe916@naver.com>
2022-09-26 00:05:41 +0900

    Converter 예제

--- src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -1,5 +1,7 @@
 package hello.typeconverter.converter;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import hello.typeconverter.type.ipPort;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +19,21 @@ public class ConverterTest {
         IntegerToStringConverter integerToStringConverter = new IntegerToStringConverter();
         String result = integerToStringConverter.convert(10);
         Assertions.assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void StringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        ipPort source = new ipPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        Assertions.assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void IpPortToString() {
+        StringToIpPortConverter con = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        ipPort result = con.convert(source);
+        Assertions.assertThat(result).isEqualTo(new ipPort("127.0.0.1", 8080));
     }
 }
