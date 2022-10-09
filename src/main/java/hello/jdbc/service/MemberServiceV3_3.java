55ef24e599ddf68079cab01bf5f4f884d1d7a4e6
qwe916 <qwe916@naver.com>
2022-10-09 09:17:32 +0900

    트랜잭션 AOP 적용(@Transactional)

--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -4,34 +4,25 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 템플릿
+ * 트랜잭션 - @Transactional AOP
  */
 @Slf4j
-public class MemberServiceV3_2 {
+public class MemberServiceV3_3 {
 
-    private final TransactionTemplate txTemplate;//트랜잭션 템플릿을 사용하기 위해 트랜잭션 매니저가 필요하다.
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
-        this.txTemplate = new TransactionTemplate(transactionManager);//트랜잭션 템플릿 주입
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
         this.memberRepository = memberRepository;
     }
 
+    @Transactional//트랜잭션 AOP 적용
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        //템플릿을 통하여 중복되는 코드를 없앨 수 있다.
-        txTemplate.executeWithoutResult((status) -> {
-            //bizLogic에 있는 SQL 예외를 execute()에서 처리를 할 수 없어 try ~catch를 사용했다.
-            try {
-                bizLogic(fromId, toId, money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
+        bizLogic(fromId, toId, money);
     }
 
     private void bizLogic( String fromId, String toId, int money) throws SQLException {
@@ -43,7 +34,6 @@ public class MemberServiceV3_2 {
         memberRepository.update(toId, toMember.getMoney() + money);
 
     }
-
 
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
