079a50c6a66ea45bcc7ca5ba1ebe636701c17e4f
qwe916 <qwe916@naver.com>
2022-10-17 13:29:20 +0900

    임베디드 모드 설정

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,15 +2,20 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV2Config.class)
+@Slf4j
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
@@ -25,4 +30,18 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+
+	@Bean
+	@Profile("test")//프로필이 test인 경우에만 bean에 등록한다.
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		//jdbc:h2:mem:db 는 임베디드 모드로 동작하는 H2를 사용할 수 있게 한다.
+		//DB_CLOSE_DELAY=-1 임베디드 모드에서는 커넥션이 끊어지면 데이터베이스도 종료되는데 이를 방지한다.
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
 }
