0866da18fe45626e10c748d748edae5a10bcc912
qwe916 <qwe916@naver.com>
2022-10-25 22:54:53 +0900

    Mybatis 제외

--- src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -9,7 +9,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 import java.util.Optional;
 
-@Mapper
+//@Mapper
 public interface ItemMapper {
     void save(Item item);
     void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto
