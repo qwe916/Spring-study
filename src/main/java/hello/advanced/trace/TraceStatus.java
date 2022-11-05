3bf6176ba78224002b92d88b6a1923e64f1c5965
qwe916 <qwe916@naver.com>
2022-11-05 23:54:38 +0900

    로그 추적기 TraceStatus 추가

---
+++ src/main/java/hello/advanced/trace/TraceStatus.java
@@ -0,0 +1,28 @@
+package hello.advanced.trace;
+
+/**
+ * 로그의 상태 정보를 나타낸다.
+ */
+public class TraceStatus {
+    private TraceId traceId;
+    private Long startTimeMs;
+    private String message;
+
+    public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
+        this.traceId = traceId;
+        this.startTimeMs = startTimeMs;
+        this.message = message;
+    }
+
+    public TraceId getTraceId() {
+        return traceId;
+    }
+
+    public Long getStartTimeMs() {
+        return startTimeMs;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
