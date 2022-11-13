11284c10bd657d0a139315bb14bd52675629b125
qwe916 <qwe916@naver.com>
2022-11-14 04:28:13 +0900

    전략을 파라미터로 넘겨 실행할 때 전략을 유용하게 변경할 수 있게 한다

---
+++ src/test/java/hello/advanced/trace/templete/strategy/code/strategy/ContextV2.java
@@ -0,0 +1,22 @@
+package hello.advanced.trace.templete.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 전략을 파라미터로 전달 받는 방식
+ * 선조립 후실행이 아닌 실행할 때마다 절략을 인수로 전달하여 더욱 유연하게 변경할 수 있게 된다.
+ */
+@Slf4j
+public class ContextV2 {
+
+    public void execute(Strategy strategy) {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        strategy.call();//위임
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+}
