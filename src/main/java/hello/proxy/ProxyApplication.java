da14e5481abf19403bdb54b309826bec0d4c6df1
qwe916 <qwe916@naver.com>
2022-11-17 23:22:48 +0900

    인터페이스가 있는 구체 클래스(수동 등록)

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,12 @@
 package hello.proxy;
 
+import hello.proxy.app.v1.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 componet scan할 때 app 디렉터리 아래의 파일들만 scan한다.
 public class ProxyApplication {
 
 	public static void main(String[] args) {
