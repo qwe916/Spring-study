63ab914d64e6805dcc205c3241337b8adf511b7d
qwe916 <qwe916@naver.com>
2022-11-22 23:54:12 +0900

    데코레이터 패턴 Test1

--- src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,6 +1,8 @@
 package hello.proxy.pureproxy.decorator;
 
+import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,14 @@ public class DecoratorPatternTest {
     void noDecorator() {
         RealComponent realComponent = new RealComponent();
         DecoratorClient client = new DecoratorClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageComponent = new MessageDecorator(realComponent);
+        DecoratorClient client = new DecoratorClient(messageComponent);
         client.execute();
     }
 
