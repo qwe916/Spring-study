42c50cd980494bbd1ac98198712897bd3a57d46e
qwe916 <qwe916@naver.com>
2022-11-24 22:49:39 +0900

    실행 시간을 측정하는 데코레이터

--- src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,6 @@
 package hello.proxy.pureproxy.decorator;
 
-import hello.proxy.pureproxy.decorator.code.Component;
-import hello.proxy.pureproxy.decorator.code.DecoratorClient;
-import hello.proxy.pureproxy.decorator.code.MessageDecorator;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +18,15 @@ public class DecoratorPatternTest {
         Component realComponent = new RealComponent();
         Component messageComponent = new MessageDecorator(realComponent);
         DecoratorClient client = new DecoratorClient(messageComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageComponent = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageComponent);
+        DecoratorClient client = new DecoratorClient(timeDecorator);
         client.execute();
     }
 
