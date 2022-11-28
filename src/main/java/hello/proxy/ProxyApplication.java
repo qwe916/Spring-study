12ea79831c5aa5e3edb90a50f2630fa11ad5ddfd
qwe916 <qwe916@naver.com>
2022-11-28 22:28:26 +0900

    JDK 동적 프록시 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,7 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxy;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.FieldLogTrace;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.boot.SpringApplication;
@@ -12,13 +13,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 /*@Import(InterfaceProxyConfig.class)*/
-@Import(ConcreteProxy.class)
+/*@Import(ConcreteProxy.class)*/
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 componet scan할 때 app 디렉터리 아래의 파일들만 scan한다.
 public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
+
 	@Bean
 	public LogTrace logTrace() {
 		return new FieldLogTrace();
