c87faa328be59db80695d34a04508b65341758ec
qwe916 <qwe916@naver.com>
2022-12-13 23:14:54 +0900

    포인트컷 재조합한 AspectV4

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,15 +2,17 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-
+@Import(AspectV4Pointcut.class)
 @SpringBootTest
 @Slf4j
 public class AopTest {
