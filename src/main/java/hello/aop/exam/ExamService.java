22b89a6ac4b359e55d3662a7f49a6fdb9d8368ee
qwe916 <qwe916@naver.com>
2022-12-19 22:31:34 +0900

    @Trace 추가하여 aop 적용

--- src/main/java/hello/aop/exam/ExamService.java
+++ src/main/java/hello/aop/exam/ExamService.java
@@ -1,11 +1,13 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Trace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ExamService {
     private final ExamRepository examRepository;
+    @Trace
     public void request(String itemId) {
         examRepository.save(itemId);
     }
