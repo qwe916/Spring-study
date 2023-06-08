dfc3dc440d2e0ec5c3d3b44f2f27e8ffde6f7676
qwe916 <qwe916@naver.com>
2023-06-08 22:38:24 +0900

    서블릿 주석 추가

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -6,19 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-@WebServlet(name = "helloServlet", urlPatterns = "/hello")//서블릿 애노테이션 name,Pattern 중복 금지
+//서블릿 애노테이션 name,Pattern 중복 금지
+//서블릿을 서블릿 컨테이너에 등록
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest requset, HttpServletResponse response) throws ServletException, IOException {
+        //서블릿이 실행되면 호출되는 메서드
         System.out.println("HelloServlet.service");
+
         System.out.println("requset = " + requset);
         System.out.println("response = " + response);
 
-        String username = requset.getParameter("username");//쿼리로 받아온 정보를 저장
+        //쿼리로 받아온 정보를 저장
+        String username = requset.getParameter("username");
         System.out.println("username = " + username);
 
-        //reposnse Content type 설정
+        //reposnse Content type 설정하여 response를 보낸다
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
         response.getWriter().write("hello" + username);
